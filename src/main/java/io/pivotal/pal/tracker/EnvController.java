13a2913acea185b63c21cc91827a97826eead57d
Winfried Klum <winfried.klum@cognizant.com>
2017-08-15 12:05:50 +0200

    Updated Concourse

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,30 @@
+package io.pivotal.pal.tracker;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+
+    private Map<String, String> env = new HashMap<>();
+
+
+    public EnvController(@Value("${PORT:NOT SET}")String port, @Value("${MEMORY_LIMIT:NOT SET}")String memory, @Value("${CF_INSTANCE_INDEX:NOT SET}")String cfIndex, @Value("${CF_INSTANCE_ADDR:NOT SET}") String cfPort )
+    {
+        env.put("PORT",port);
+        env.put("MEMORY_LIMIT",memory);
+        env.put("CF_INSTANCE_INDEX",cfIndex);
+        env.put("CF_INSTANCE_ADDR",cfPort);
+    }
+
+    @GetMapping("/env")
+    public Map<String,String> getEnv(){
+        return env;
+    }
+}
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    String msg;
+
+
+    public WelcomeController(@Value("${welcome_message}")String message){
+        msg = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return msg;
     }
 }
